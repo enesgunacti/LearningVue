c428706620a3a01e7ac7967347009dd8adad4a46
enesgunacti <gnctenes@gmail.com>
2022-09-22 17:56:50 +0300

    Components

---
+++ VueCli/components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+import Greeting from "./components/Greeting.vue";
+export default {
+  name: "App",
+  components: {
+    Greeting: Greeting,
+  },
+};
+</script>
+
