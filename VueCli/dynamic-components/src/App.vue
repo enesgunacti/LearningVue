b4cb55ce307663e8961922bf2f9559b93cbafb83
enesgunacti <gnctenes@gmail.com>
2022-09-23 14:50:37 +0300

    ..

---
+++ VueCli/dynamic-components/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <select name="" id="">
+    <option value="TestHome">Home</option>
+    <option value="TestAbout">About</option>
+  </select>
+  <TestHome />
+</template>
+
+
+
+<script>
+import TestHome from "./components/TestAbout.vue";
+import TestAbout from "./components/TestHome.vue";
+export default {
+  name: "App",
+  components: {
+    TestHome,
+    TestAbout,
+  },
+};
+</script>
+
